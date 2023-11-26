7a71a7cc890b946ee5c1dafa29d7b09d003ca0e0
halidak <halida.karisik6@gmail.com>
2023-11-26 21:43:52 +0100

    404 page

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
- * @package mms
+ * @package MultimedijalniSistemi
  */
 
 get_header();
@@ -14,11 +14,11 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mms' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'multimedijalnisistemi' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'mms' ); ?></p>
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'multimedijalnisistemi' ); ?></p>
 
 					<?php
 					get_search_form();
@@ -27,7 +27,7 @@ get_header();
 					?>
 
 					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'mms' ); ?></h2>
+						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'multimedijalnisistemi' ); ?></h2>
 						<ul>
 							<?php
 							wp_list_categories(
@@ -45,8 +45,8 @@ get_header();
 
 					<?php
 					/* translators: %1$s: smiley */
-					$mms_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'mms' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$mms_archive_content" );
+					$multimedijalnisistemi_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'multimedijalnisistemi' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$multimedijalnisistemi_archive_content" );
 
 					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
